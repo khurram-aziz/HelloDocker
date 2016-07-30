f7af250cc9471159adfa6dd3c211c1d333e69b78
khurram-aziz <khuziz@hotmail.com>
2016-07-31 00:24:08 +0500

    Initial Commit

---
+++ DockerBuild.bat
@@ -0,0 +1,5 @@
+docker build -t khurramaziz/gluster:3.5.2 -f Dockerfile.gluster .
+docker build -t khurramaziz/gluster:3.5.2-samba -f Dockerfile.glustersamba .
+docker build -t khurramaziz/mongo -f Dockerfile.mongodb .
+docker build -t khurramaziz/node -f Dockerfile.node .
+docker build -t khurramaziz/ucarp -f Dockerfile.ucarp .
