59dd9669edc032d033dbfe13db6642a14b1774d6
khurram-aziz <khuziz@hotmail.com>
2016-08-10 22:06:17 +0500

    August

--- build-dockerimages.bat
+++ build-dockerimages.bat
@@ -5,6 +5,6 @@ docker build -t khurramaziz/gluster:3.7.3-samba -f Dockerfile.glustersamba-3.7 .
 docker tag khurramaziz/gluster:3.7.3-samba khurramaziz/gluster:latest
 
 docker build -t khurramaziz/linuxtools -f Dockerfile.linuxtools .
-docker build -t khurramaziz/mongo -f Dockerfile.mongodb .
+docker build -t khurramaziz/mongo -f Dockerfile.mongo .
 docker build -t khurramaziz/node -f Dockerfile.node .
 docker build -t khurramaziz/ucarp -f Dockerfile.ucarp .
