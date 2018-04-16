791156cad82e5ade932027195c263ae437ec38e8
khurram <khuziz@hotmail.com>
2018-04-16 14:15:00 +0500

    Prometheus

---
+++ Prometheus/Net4Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace Net4Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string url = @"http://localhost:8000";
+                var config = new HttpSelfHostConfiguration(url);
+                config.Routes.MapHttpRoute("API Default",
+                    "api/{controller}/{action}/{id}",
+                    new { id = RouteParameter.Optional });
+                config.Routes.MapHttpRoute("Status", "status", new { controller = "Status" });
+                config.Routes.MapHttpRoute("Metrics", "metrics", new { controller = "Status", action = "Metrics" });
+                HttpSelfHostServer server = new HttpSelfHostServer(config);
+                server.OpenAsync().Wait();
+
+                Console.WriteLine("Web server started at {0}", url);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to setup web server");
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine("Press ENTER to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Prometheus/Net4Console/StatusController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace Net4Console
+{
+    public class StatusController : ApiController
+    {
+        public static int Counter = 0;
+        public static int Gauge = 0;
+
+        public HttpResponseMessage Get()
+        {
+            var html = new StringBuilder();
+
+            html.Append(@"<html><head><title>Status</title><meta http-equiv=""refresh"" content=""30""></head><body>");
+            html.Append(string.Format("<p>Machine Name: {0}<br>Operating System: {1}</p>", Environment.MachineName, Environment.OSVersion));
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new StringContent(html.ToString(), Encoding.UTF8, "text/html");
+            return resp;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Metrics()
+        {
+            StatusController.Counter++;
+            StatusController.Gauge = new Random().Next(0, 1000);
+
+            var sb = new StringBuilder();
+
+            sb.Append(string.Format(@"machine{{name=""{1}"",os=""{2}""}} 1{0}", Environment.NewLine, Environment.MachineName, Environment.OSVersion));
+            sb.Append(string.Format(@"custom_counter {1}{0}custom_gauge {2}", Environment.NewLine, StatusController.Counter, StatusController.Gauge));
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new StringContent(sb.ToString(), Encoding.UTF8, "text/plain"); //"text/plain; version=0.0.4" gives invalid
+            return resp;
+        }
+    }
+}
