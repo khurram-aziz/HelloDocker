ba2036081102c0faa183a4cd0f66bf0e84d8b893
khurram <khuziz@hotmail.com>
2018-04-16 22:39:51 +0500

    NET Core app with prometheus-net

---
+++ Prometheus/NetCoreConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using Prometheus;
+using System;
+using System.Threading;
+
+namespace NetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = Metrics.CreateCounter("custom_counter", "some help about this");
+            var gauge = Metrics.CreateGauge("custom_gauge", "some help about this");
+
+            var thread = new Thread(new ThreadStart(() =>
+            {
+                gauge.Set(new Random().Next(0, 1000));
+                counter.Inc();
+                Thread.Sleep(1000);
+            }));
+
+            var label = Metrics.CreateGauge("machine", "some help about this", new GaugeConfiguration()
+            {
+                LabelNames = new[] { "name", "os" }
+            });
+            label.WithLabels(Environment.MachineName, Environment.OSVersion.ToString()).Set(1);
+
+            var metricServer = new MetricServer(port: 8000); //by default it publishes at /metrics
+            metricServer.Start();
+
+            thread.Start();
+
+            Console.WriteLine("Press ENTER to exit...");
+            Console.ReadLine();
+
+            try
+            {
+                thread.Abort();
+            }
+            catch { }
+            try
+            {
+                metricServer.Stop();
+            }
+            catch { }
+        }
+    }
+}
