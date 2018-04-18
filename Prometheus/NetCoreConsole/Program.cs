3c9fe4dc782f872c76fe7206946ba4c144020990
khurram <khuziz@hotmail.com>
2018-04-18 14:46:49 +0500

    netcoreconsole + bauth

--- Prometheus/NetCoreConsole/Program.cs
+++ Prometheus/NetCoreConsole/Program.cs
@@ -1,13 +1,30 @@
 ﻿using Prometheus;
 using System;
+using System.Runtime.Loader;
 using System.Threading;
 
 namespace NetCoreConsole
 {
     class Program
     {
+        static bool keepRunning = false;
+        static void SigTermEventHandler(AssemblyLoadContext obj)
+        {
+            Program.keepRunning = false;
+            Console.WriteLine("Unloading...");
+        }
+
+        static void CancelHandler(object sender, ConsoleCancelEventArgs e)
+        {
+            Program.keepRunning = false;
+            Console.WriteLine("Exiting...");
+        }
+
         static void Main(string[] args)
         {
+            AssemblyLoadContext.Default.Unloading += SigTermEventHandler; //register sigterm event handler. Don't forget to import System.Runtime.Loader!
+            Console.CancelKeyPress += CancelHandler; //register sigint event handler
+
             var counter = Metrics.CreateCounter("custom_counter", "some help about this");
             var gauge = Metrics.CreateGauge("custom_gauge", "some help about this");
 
@@ -29,8 +46,13 @@ namespace NetCoreConsole
 
             thread.Start();
 
-            Console.WriteLine("Press ENTER to exit...");
-            Console.ReadLine();
+            Program.keepRunning = true;
+
+            do
+            {
+                Thread.Sleep(1000);
+                //Console.WriteLine("I am alive...");
+            } while (Program.keepRunning);
 
             try
             {
